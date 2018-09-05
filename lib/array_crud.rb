446ba3c76787d2dfa48f3b10d7e262a72a1d1f3e
Sean Sutton Jr <sutton_mjbc@icloud.com>
2018-09-05 17:33:00 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,8 +7,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  array = [1, 2, 3, 4]
-  element = 5
+  array = ["wow", "I", "am", "really", "learning"]
+  element = "arrays!"
   array << element
 end
 
